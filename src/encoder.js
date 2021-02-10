aec2930506c484f95620cde4403db62ac08072a9
Carlos Flores <carlos@dharma.io>
2021-02-09 16:53:25 -0800

    add call encoding

---
+++ src/encoder.js
@@ -0,0 +1,534 @@
+const { Validator } = require('./Validator');
+const hre = require("hardhat");
+const ethers = hre.ethers;
+
+const ERC20_ABI = [
+    {
+        constant: true,
+        inputs: [],
+        name: "name",
+        outputs: [
+            {
+                name: "",
+                type: "string",
+            },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        constant: false,
+        inputs: [
+            {
+                name: "_spender",
+                type: "address",
+            },
+            {
+                name: "_value",
+                type: "uint256",
+            },
+        ],
+        name: "approve",
+        outputs: [
+            {
+                name: "",
+                type: "bool",
+            },
+        ],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+    },
+    {
+        constant: true,
+        inputs: [],
+        name: "totalSupply",
+        outputs: [
+            {
+                name: "",
+                type: "uint256",
+            },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        constant: false,
+        inputs: [
+            {
+                name: "_from",
+                type: "address",
+            },
+            {
+                name: "_to",
+                type: "address",
+            },
+            {
+                name: "_value",
+                type: "uint256",
+            },
+        ],
+        name: "transferFrom",
+        outputs: [
+            {
+                name: "",
+                type: "bool",
+            },
+        ],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+    },
+    {
+        constant: true,
+        inputs: [],
+        name: "decimals",
+        outputs: [
+            {
+                name: "",
+                type: "uint8",
+            },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        constant: true,
+        inputs: [
+            {
+                name: "_owner",
+                type: "address",
+            },
+        ],
+        name: "balanceOf",
+        outputs: [
+            {
+                name: "balance",
+                type: "uint256",
+            },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        constant: true,
+        inputs: [],
+        name: "symbol",
+        outputs: [
+            {
+                name: "",
+                type: "string",
+            },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        constant: false,
+        inputs: [
+            {
+                name: "_to",
+                type: "address",
+            },
+            {
+                name: "_value",
+                type: "uint256",
+            },
+        ],
+        name: "transfer",
+        outputs: [
+            {
+                name: "",
+                type: "bool",
+            },
+        ],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+    },
+    {
+        constant: true,
+        inputs: [
+            {
+                name: "_owner",
+                type: "address",
+            },
+            {
+                name: "_spender",
+                type: "address",
+            },
+        ],
+        name: "allowance",
+        outputs: [
+            {
+                name: "",
+                type: "uint256",
+            },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        payable: true,
+        stateMutability: "payable",
+        type: "fallback",
+    },
+    {
+        anonymous: false,
+        inputs: [
+            {
+                indexed: true,
+                name: "owner",
+                type: "address",
+            },
+            {
+                indexed: true,
+                name: "spender",
+                type: "address",
+            },
+            {
+                indexed: false,
+                name: "value",
+                type: "uint256",
+            },
+        ],
+        name: "Approval",
+        type: "event",
+    },
+    {
+        anonymous: false,
+        inputs: [
+            {
+                indexed: true,
+                name: "from",
+                type: "address",
+            },
+            {
+                indexed: true,
+                name: "to",
+                type: "address",
+            },
+            {
+                indexed: false,
+                name: "value",
+                type: "uint256",
+            },
+        ],
+        name: "Transfer",
+        type: "event",
+    },
+];
+
+class Encoder {
+
+    static async encode(actionScriptName, variables, wallet) {
+        const encoder = new Encoder(actionScriptName, variables, wallet);
+
+        await encoder.parseActionScriptDefinitions();
+        await encoder.constructCallsAndResultsFormat();
+
+        return encoder.calls;
+    }
+
+    constructor(actionScriptName, variables, wallet) {
+        this.actionScriptName = actionScriptName;
+        this.variables = variables;
+        this.variables["wallet"] = wallet;
+    }
+
+    async constructCallsAndResultsFormat() {
+        const script = await Validator.getActionScript(this.actionScriptName);
+
+        this.calls = [];
+        this.callABIs = [];
+        this.resultToParse = {};
+
+        const contracts = Object.fromEntries(
+            Object.entries(this.targetContracts).map(([name, value]) => {
+                return [name, new ethers.Contract(value.address, value.abi, ethers.provider)];
+            })
+        );
+
+        const { actions } = script;
+
+        for (let [callIndex, action] of Object.entries(actions)) {
+            const appliedArgs = [];
+
+            // parse calls
+            const splitActionCalls = action.split(" => ")[0];
+
+            let [contractName, functionName, ...args] = splitActionCalls.split(
+                " "
+            );
+
+            if (contractName === "ETHER") {
+                args = functionName.split(":");
+            }
+
+            let payableArg = 0;
+            let appliedPayableArg = 0;
+            if (functionName.includes(":")) {
+                payableArg = functionName.split(":")[1];
+                functionName = functionName.split(":")[0];
+
+                if (payableArg in this.variables) {
+                    appliedPayableArg = this.variables[payableArg];
+                } else if (payableArg in this.targetContracts) {
+                    appliedPayableArg = this.targetContracts[payableArg]
+                        .address;
+                } else {
+                    appliedPayableArg = payableArg;
+                }
+            }
+
+            for (let arg of args) {
+                if (arg in this.variables) {
+                    const appliedArg = this.variables[arg];
+                    appliedArgs.push(appliedArg);
+                } else if (arg in this.targetContracts) {
+                    appliedArgs.push(this.targetContracts[arg].address);
+                } else {
+                    appliedArgs.push(arg);
+                }
+            }
+
+            let targetFunction = this.targetFunctions[functionName];
+
+            if (!!targetFunction && targetFunction.includes(":")) {
+                targetFunction = targetFunction.split(":")[0];
+            }
+
+            if (contractName === "ETHER") {
+                this.calls.push({
+                    to: appliedArgs[0],
+                    value: appliedArgs[1],
+                    data: "0x",
+                });
+
+                this.callABIs.push({
+                    payable: true,
+                    stateMutability: "payable",
+                    type: "fallback",
+                });
+            } else if (targetFunction === "fallback") {
+                const contract = contracts[contractName];
+                const to = contract.address;
+
+                this.calls.push({
+                    to,
+                    value: appliedPayableArg,
+                    data: "0x",
+                });
+
+                this.callABIs.push({
+                    payable: true,
+                    stateMutability: "payable",
+                    type: "fallback",
+                });
+            } else {
+                const contract = contracts[contractName];
+
+                const to = contract.address;
+                const data = contract.interface.encodeFunctionData(targetFunction, appliedArgs);
+
+                this.calls.push({
+                    to,
+                    value: appliedPayableArg,
+                    data,
+                });
+
+                const callABIIndex = this.targetContracts[contractName].abi
+                    .map(f => f.name)
+                    .indexOf(targetFunction);
+
+                if (callABIIndex === -1) {
+                    throw new Error("Could not find function ABI");
+                }
+
+                this.callABIs.push(
+                    this.targetContracts[contractName].abi[callABIIndex]
+                );
+
+                // parse results
+                const splitActionResults = action.split(" => ");
+
+                if (splitActionResults.length === 1) {
+                    continue;
+                }
+
+                const results = splitActionResults[1].split(" ");
+
+                for (let [argumentIndex, result] of Object.entries(results)) {
+                    if (!(callIndex in this.resultToParse)) {
+                        this.resultToParse[callIndex] = {};
+                    }
+
+                    this.resultToParse[callIndex][argumentIndex] = result;
+                }
+            }
+        }
+    }
+
+    async parseActionScriptDefinitions() {
+        const script = await Validator.getActionScript(this.actionScriptName);
+        const definitions = script.definitions || [];
+
+        this.targetContracts = {};
+        this.targetFunctions = {
+            transfer: "transfer",
+            transferFrom: "transferFrom",
+            balanceOf: "balanceOf",
+            approve: "approve",
+            totalSupply: "totalSupply",
+            allowance: "allowance",
+        };
+        for (let definition of definitions) {
+            const splitDefinition = definition.split(" "); // Contract | Token | Function | Action
+
+            const splitDefinitionByOutputs = definition.split(" => ");
+
+            const outputDefinitions =
+                splitDefinitionByOutputs.length > 1
+                    ? splitDefinitionByOutputs[1].split(" ")
+                    : [];
+            const type = splitDefinition[0];
+
+            let name;
+            let address;
+            let functionName;
+            let contractName;
+            let candidateAddress;
+
+            switch (type) {
+                case "Contract":
+                    name = splitDefinition[1];
+
+                    candidateAddress = splitDefinition[2];
+
+                    if (candidateAddress in this.variables) {
+                        // TODO validate 'address' type:
+                        address = this.variables[candidateAddress];
+                    } else {
+                        address = candidateAddress;
+                    }
+
+                    this.targetContracts[name] = {
+                        address,
+                        abi: [],
+                        namedFunctions: {},
+                    };
+                    break;
+                case "Token":
+                    name = splitDefinition[1];
+
+                    candidateAddress = splitDefinition[2];
+
+                    if (candidateAddress in this.variables) {
+                        // TODO validate 'address' type:
+                        address = this.variables[candidateAddress];
+                    } else {
+                        address = candidateAddress;
+                    }
+
+                    this.targetContracts[name] = {
+                        address,
+                        abi: ERC20_ABI,
+                        namedFunctions: {},
+                    };
+
+                    break;
+                case "Function":
+                    functionName = splitDefinition[1];
+                    contractName = splitDefinition[2];
+
+                    let payable = false;
+
+                    let functionSignature = splitDefinition[3];
+
+                    if (functionSignature.endsWith(":payable")) {
+                        payable = true;
+                        functionSignature = functionSignature.slice(0, -8);
+                    }
+
+                    let functionABI = {};
+                    let contractFunctionName;
+                    if (functionSignature === "fallback") {
+                        functionSignature = "()";
+                        functionABI = {
+                            payable,
+                            stateMutability: payable ? "payable" : "nonpayable",
+                            type: "fallback",
+                        };
+                        contractFunctionName = "fallback";
+                    } else {
+                        contractFunctionName = functionSignature.split("(")[0];
+
+                        let functionDefinitionWithArguments =
+                            splitDefinitionByOutputs[0];
+                        if (
+                            functionDefinitionWithArguments.endsWith(":payable")
+                        ) {
+                            functionDefinitionWithArguments = functionDefinitionWithArguments.slice(
+                                0,
+                                -8
+                            );
+                        }
+
+                        const allContractFunctionArgumentString = functionDefinitionWithArguments
+                            .split("(")[1]
+                            .slice(0, -1);
+
+                        const contractFunctionArguments =
+                            allContractFunctionArgumentString.length > 0
+                                ? allContractFunctionArgumentString.split(",")
+                                : null;
+
+                        functionABI = {
+                            constant: false,
+                            name: contractFunctionName,
+                            outputs: !!outputDefinitions
+                                ? Object.entries(outputDefinitions).map(
+                                    ([i, returnValue]) => ({
+                                        name: `${contractFunctionName}ReturnValue${i}`,
+                                        type: returnValue.trim(" "),
+                                        internalType: returnValue.trim(" "),
+                                    })
+                                )
+                                : [],
+                            payable,
+                            stateMutability: payable ? "payable" : "nonpayable",
+                            type: "function",
+                            inputs: !!contractFunctionArguments
+                                ? Object.entries(contractFunctionArguments).map(
+                                    ([i, arg]) => ({
+                                        name: `${contractFunctionName}Argument${i}`,
+                                        type: arg.trim(" "),
+                                        internalType: arg.trim(" "),
+                                    })
+                                )
+                                : [],
+                        };
+                    }
+
+                    this.targetContracts[contractName].abi.push(functionABI);
+
+                    this.targetFunctions[functionName] = contractFunctionName;
+
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+
+
+module.exports = {
+    Encoder
+};
--- src/setup.js
+++ src/setup.js
@@ -1,22 +1,27 @@
 const hre = require("hardhat");
 const ethers = hre.ethers;
 const { Validator } = require('./Validator');
+const { Encoder } = require("./Encoder");
 
 async function main() {
-  const signers = await ethers.getSigners();
-  const signer = signers[0];
+	const signers = await ethers.getSigners();
+	const signer = signers[0];
 
-  const Wallet = await ethers.getContractFactory("Wallet");
-  const wallet = await Wallet.deploy(signer.address);
+	const Wallet = await ethers.getContractFactory("Wallet");
+	const wallet = await Wallet.deploy(signer.address);
 
-  const actionScript = await Validator.getActionScript("SWAP_ON_CURVE");
-  const { definitions, inputs } = actionScript;
+	const actionScript = await Validator.getActionScript("SWAP_ON_CURVE");
+	const { definitions, inputs } = actionScript;
 
-  const VARIABLES = {
-  	soldTokenAmount: 100,
-  	soldTokenAddress: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
-  	boughtTokenAddress: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-  }
+	const VARIABLES = {
+		soldTokenAmount: "1000000000000000000",
+		soldTokenAddress: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+		boughtTokenAddress: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+		curvePoolAddress: "0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7",
+		soldTokenIndex: 0,
+		boughtTokenIndex: 1,
+		minimumBoughtTokenAmount: "900000",
+	};
 
 	const tokenDefinitions = Object.fromEntries(
 		definitions
@@ -25,101 +30,92 @@ async function main() {
 			.map(d => [d[1], d[2] in VARIABLES ? VARIABLES[d[2]] : d[2]])
 	);
 
-  const inputTokens = {};
-  for (let inputObjects of inputs) {
-    const [tokenName, inputVariable] = Object.entries(inputObjects).pop();
-      const inputValue = inputVariable in VARIABLES
-    	? VARIABLES[inputVariable]
-    	: inputVariable;
+	const inputTokens = {};
+	for (let inputObjects of inputs) {
+		const [tokenName, inputVariable] = Object.entries(inputObjects).pop();
+		const inputValue = inputVariable in VARIABLES
+			? VARIABLES[inputVariable]
+			: inputVariable;
 
-	  if (tokenName in inputTokens) {
-		inputTokens[tokenName] = inputTokens[tokenName].add(ethers.BigNumber.from(inputValue));
-	  } else {
-		inputTokens[tokenName] = ethers.BigNumber.from(inputValue);
-	  }
-  }
+		if (tokenName in inputTokens) {
+			inputTokens[tokenName] = inputTokens[tokenName].add(ethers.BigNumber.from(inputValue));
+		} else {
+			inputTokens[tokenName] = ethers.BigNumber.from(inputValue);
+		}
+	}
 
 
-  for (let [tokenName, amount] of Object.entries(inputTokens)) {
-  	const balance = await signer.getBalance();
-  	if (tokenName === 'ETHER') {
-  		if (balance.lt(amount)) {
-	    	throw new Error(
-	    		`Ether input amount ${amount} exceeds available balance ${balance}`
-	    	);
-	    }
+	for (let [tokenName, amount] of Object.entries(inputTokens)) {
+		const balance = await signer.getBalance();
+		if (tokenName === 'ETHER') {
+			if (balance.lt(amount)) {
+				throw new Error(
+					`Ether input amount ${amount} exceeds available balance ${balance}`
+				);
+			}
 
-	    const tx = await signer.sendTransaction({
-	    	to: wallet.address,
-	    	value: amount,
-	    });
+			const tx = await signer.sendTransaction({
+				to: wallet.address,
+				value: amount,
+			});
 
-	    const walletBalance = await ethers.provider.getBalance(wallet.address);
-  		if (walletBalance.lt(amount)) {
-	    	throw new Error(
-	    		`Ether input amount ${amount} exceeds wallet balance ${walletBalance}`
-	    	);
-	    }
-  	} else {
-  		const tokenAddress = tokenDefinitions[tokenName];
-  		const token = new ethers.Contract(
-  			tokenAddress,
-  			["function balanceOf(address account) view returns (uint256 balance)"],
-  			ethers.provider,
-  		);
-  		const router = new ethers.Contract(
-  			"0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
-  			["function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline) payable returns (uint[] memory amounts)"],
-  			signer
-  		);
-  		const tx = await router.swapETHForExactTokens(
-  			amount,
-  			["0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", tokenAddress],
-  			wallet.address,
-  			99999999999999,
-  			{value: balance.div(2)}
-  		);
-  		const walletBalance = await token.balanceOf(wallet.address);
-  		if (walletBalance.lt(amount)) {
-	    	throw new Error(
-	    		`Token input amount ${amount} exceeds wallet balance ${walletBalance}`
-	    	);
-	    }
-  	}
-  }
+			const walletBalance = await ethers.provider.getBalance(wallet.address);
+			if (walletBalance.lt(amount)) {
+				throw new Error(
+					`Ether input amount ${amount} exceeds wallet balance ${walletBalance}`
+				);
+			}
+		} else {
+			const tokenAddress = tokenDefinitions[tokenName];
+			const token = new ethers.Contract(
+				tokenAddress,
+				["function balanceOf(address account) view returns (uint256 balance)"],
+				ethers.provider,
+			);
+			const router = new ethers.Contract(
+				"0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+				["function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline) payable returns (uint[] memory amounts)"],
+				signer
+			);
+			const tx = await router.swapETHForExactTokens(
+				amount,
+				["0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", tokenAddress],
+				wallet.address,
+				99999999999999,
+				{value: balance.div(2)}
+			);
+			const walletBalance = await token.balanceOf(wallet.address);
+			if (walletBalance.lt(amount)) {
+				throw new Error(
+					`Token input amount ${amount} exceeds wallet balance ${walletBalance}`
+				);
+			}
+		}
+	}
 
-  // TODO: feed variables into Encoder in order to get calls
-  const calls = [{
-  	to: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-  	value: 100,
-  	data: "0xd0e30db0",
-  }];
+	const calls = await Encoder.encode("SWAP_ON_CURVE", VARIABLES, wallet.address);
 
-  let simulation = await wallet.callStatic.simulate(calls);
-  console.log({simulation});
+	let simulation = await wallet.callStatic.simulate(calls);
 
-  let execution = await wallet.execute(calls);
-  execution = await execution.wait();
-  const logs = execution.logs;
+	let execution = await wallet.execute(calls);
+	execution = await execution.wait();
+	const logs = execution.logs;
 
-  const events = [];
-  for (let log of logs) {
-  	let currentEvent = null;
-  	try {
-  		currentEvent = wallet.interface.parseLog(log);
-  	} catch (error) {
-  		console.error('ERROR');
-  	}
-  	events.push(currentEvent);
-  }
-
-  console.log({events});
-
+	const events = [];
+	for (let log of logs) {
+		let currentEvent = null;
+		try {
+			currentEvent = wallet.interface.parseLog(log);
+		} catch (error) {
+			console.error('ERROR');
+		}
+		events.push(currentEvent);
+	}
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
+	.then(() => process.exit(0))
+	.catch(error => {
+		console.error(error);
+		process.exit(1);
+	});
