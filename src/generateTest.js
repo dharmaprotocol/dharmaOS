db971b2c9d587660b9fa49cb7c055a4ce1c41f19
0age <0age@protonmail.com>
2021-02-11 09:44:08 -0800

    add writer to basic test generator

--- src/generateTest.js
+++ src/generateTest.js
@@ -1,18 +1,20 @@
+const fs = require('fs');
+const path = require('path');
 const ethers = require("hardhat").ethers;
 const YAML = require('yaml');
 const { Validator } = require("./Validator");
 const { evaluate } = require("./evaluate");
 
 const generateSinglePassingTest = async (actionScriptName, variables, actionScriptTestName = null) => {
-	const blockNumber = await ethers.provider.getBlockNumber();
+    const blockNumber = await ethers.provider.getBlockNumber();
 
-	let testName = actionScriptTestName;
-	if (testName === null) {
-		const { summary } = await Validator.getActionScript(actionScriptName);
-		testName = summary;
-	}
+    let testName = actionScriptTestName;
+    if (testName === null) {
+        const { summary } = await Validator.getActionScript(actionScriptName);
+        testName = summary;
+    }
 
-	console.log(`Generating single test for ${actionScriptName}: ${testName}`);
+    console.log(`Generating single test for ${actionScriptName}: ${testName}`);
 
     const { success, results, events } = await evaluate(
         actionScriptName,
@@ -22,24 +24,60 @@ const generateSinglePassingTest = async (actionScriptName, variables, actionScri
 
     delete variables.wallet;
 
-    const test = YAML.stringify({
-    	name: actionScriptName,
-    	blockNumber,
-    	tests: [{
-    		name: testName,
-    		success: true,
-    		variables,
-    		results,
-    		events,
-    	}],
+    return YAML.stringify({
+        name: actionScriptName,
+        blockNumber,
+        tests: [{
+            name: testName,
+            success: true,
+            variables,
+            results,
+            events,
+        }],
     });
-
-    console.log(test);
 }
 
-generateSinglePassingTest(
-	"DEPOSIT_ETH_TO_COMPOUND",
-	{
-		suppliedETHAmount: ethers.utils.parseEther("1").toString(),
-	}
-);
+const writeTestToFile = async (actionScriptName, testYAML, category) => {
+    const testDir = path.resolve(__dirname, '../action-script-tests');
+    try {
+      await fs.promises.mkdir(testDir);
+    } catch (e) {}
+
+    const categoryDir = path.resolve(testDir, category);
+    try {
+      await fs.promises.mkdir(categoryDir);
+    } catch (e) {}
+
+    fs.writeFileSync(
+        path.resolve(categoryDir, `${actionScriptName}.yaml`),
+        testYAML,
+        'utf8',
+        (err) => {
+            if (err) {
+                console.error(err);
+            }
+        }
+    );
+}
+
+const createSinglePassingTestAndWrite = async (category, actionScriptName, variables) => {
+    const testYAML = await generateSinglePassingTest(actionScriptName, variables);
+    console.log(testYAML);
+
+    await writeTestToFile(actionScriptName, testYAML, category);
+    console.log(`Created test: ${category}/${actionScriptName}.yaml`);
+
+    process.exit(0);
+}
+
+const main = async () => {
+    await createSinglePassingTestAndWrite(
+        "weth",
+        "WRAP_ETHER",
+        {
+            amount: ethers.utils.parseEther("1").toString(),
+        },
+    );
+}
+
+main();
