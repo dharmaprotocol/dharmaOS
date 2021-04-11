5388f1288be0c5fc6822e16508fc3a1d83b0175f
0age <0age@protonmail.com>
2021-04-10 21:25:18 -0700

    skip tests against v10 for threads greater than 0

--- test/1.js
+++ test/1.js
@@ -1,3 +1,13 @@
-if (typeof process.env.CI_THREAD === "undefined" || process.env.CI_THREAD === "1") {
+if (
+	typeof process.env.CI_THREAD !== "undefined" && (
+		process.env.CI_THREAD === "1" && !process.version.startsWith("v15")
+	)
+) {
+	console.log("Skipping thread 1 tests for < v15!");
+} else if (
+	typeof process.env.CI_THREAD === "undefined" || (
+		process.env.CI_THREAD === "1" && process.version.startsWith("v15")
+	)
+) {
 	require("../src/test-template")(1);
 }
--- test/2.js
+++ test/2.js
@@ -1,3 +1,13 @@
-if (typeof process.env.CI_THREAD === 'undefined' || process.env.CI_THREAD === "2") {
+if (
+	typeof process.env.CI_THREAD !== "undefined" && (
+		process.env.CI_THREAD === "2" && !process.version.startsWith("v15")
+	)
+) {
+	console.log("Skipping thread 2 tests for < v15!");
+} else if (
+	typeof process.env.CI_THREAD === "undefined" || (
+		process.env.CI_THREAD === "2" && process.version.startsWith("v15")
+	)
+) {
 	require("../src/test-template")(2);
 }
