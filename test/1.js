260f5f93d004a758021283559bf7127684156fa0
0age <0age@protonmail.com>
2021-04-10 21:07:20 -0700

    try strings for CI_THREAD comparison

--- test/1.js
+++ test/1.js
@@ -1,3 +1,3 @@
-if (typeof process.env.CI_THREAD === 'undefined' || process.env.CI_THREAD === 1) {
+if (typeof process.env.CI_THREAD === "undefined" || process.env.CI_THREAD === "1") {
 	require("../src/test-template")(1);
 }
