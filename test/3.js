59cea6deabd4840634e62f37a262934212943307
0age <0age@protonmail.com>
2021-04-10 21:32:47 -0700

    break out "legacy" test into independent github action

--- test/3.js
+++ test/3.js
@@ -1,13 +1,3 @@
-if (
-	typeof process.env.CI_THREAD !== "undefined" && (
-		process.env.CI_THREAD === "3" && !process.version.startsWith("v15")
-	)
-) {
-	console.log("Skipping thread 3 tests for < v15!");
-} else if (
-	typeof process.env.CI_THREAD === "undefined" || (
-		process.env.CI_THREAD === "3" && process.version.startsWith("v15")
-	)
-) {
+if (typeof process.env.CI_THREAD === 'undefined' || process.env.CI_THREAD === "3") {
 	require("../src/test-template")(3);
 }
